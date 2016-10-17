77fdf4753c8e47de69bf614e80d4191ebdad739c
Ball Weera Kasetsin <wkasetsin@gmail.com>
2016-10-18 05:33:54 +0700

    Create api: GET /todo/list

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+app.get('/todo/list', (req, res) => {
+  res.json([
+    { id: 1, title: 'Learn NodeJS', completed: false },
+    { id: 2, title: 'Read a article', completed: true }
+  ])
+})
+
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from NodeJS API' })
 })
