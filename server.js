d0f4da3d28c2fe0a3fe072ac8769eba0361e6097
Ball Weera Kasetsin <wkasetsin@gmail.com>
2016-10-18 05:45:28 +0700

    Create api: POST /todo/create

--- server.js
+++ server.js
@@ -10,17 +10,26 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
 
+let todos = [
+  { id: 1, title: 'Learn NodeJS', completed: false },
+  { id: 2, title: 'Read a article', completed: true }
+]
+
 app.get('/todo/list', (req, res) => {
-  res.json([
-    { id: 1, title: 'Learn NodeJS', completed: false },
-    { id: 2, title: 'Read a article', completed: true }
-  ])
+  res.json(todos)
+})
+
+app.post('/todo/create', (req, res) => {
+  let todo = req.body
+
+  todo.id = 3
+  todos.push(todo)
+  res.json(todo)
 })
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from NodeJS API' })
 })
-
 
 const port = process.env.PORT || 9090
 app.listen(port, () => {
